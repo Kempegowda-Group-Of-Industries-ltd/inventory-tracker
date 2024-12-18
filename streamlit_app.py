5632e04b7c50729a66edd99e51a510b5201efa91
KempeGowda Group Of Industries Ltd <kempegowdagroupofindustriesltd@gmail.com>
2024-12-17 20:03:37 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,7 +171,7 @@ def update_data(conn, df, changes):
 
 # Set the title that appears at the top of the page.
 """
-# :secure: AVANI 
+# :shopping_bags: AVANI 
 
 **Welcome to KGI-Corner Store's intentory tracker!**
 This page reads and writes directly from/to our inventory database.
