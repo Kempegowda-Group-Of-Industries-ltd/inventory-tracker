703968b59274ee2270847507e1ef7aadca486916
KempeGowda Group Of Industries Ltd <kempegowdagroupofindustriesltd@gmail.com>
2025-04-30 22:29:49 -0400

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,13 @@ import pandas as pd
 
 import streamlit as st
 
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title="AVANI",
+    page_icon=":progress:",  # This is an emoji shortcode. Could be a URL too.
+)
+
+
 def login():
     if "authenticated" not in st.session_state:
         st.session_state.authenticated = False
@@ -33,12 +40,6 @@ if not login():
 
 
 
-
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title="AVANI",
-    page_icon=":progress:",  # This is an emoji shortcode. Could be a URL too.
-)
 
 
 # -----------------------------------------------------------------------------
